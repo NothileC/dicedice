a7af90da129cf1120e6f40e2e3c713ad8a2e3d72
NothileC <nothilecmhlanga@outlook.com>
2024-03-03 20:26:21 +0800

    updating js

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ function rollDice(scoreId){
     }
 }
 
+
+//Logic for the winner
 function displayWinner(){
     if(parseInt(document.getElementById("score1").innerHTML) > parseInt(document.getElementById("score2").innerHTML)){
         document.querySelector('h1').innerHTML = 'The winner is User 1';
@@ -42,7 +44,16 @@ function displayWinner(){
     }
 }
 
+//displaying dice image 
 function displayDice(num){
-    let imageName = "/cards/dice" + num + ".jpeg";
-    document.getElementById("dice-image").src = imageName;
+    let imageName = "cards/dice" + num + ".jpeg";
+    
+    document.getElementById("img").src = imageName;
+}
+//reset button
+function newGame(){
+    document.getElementById("counter").innerHTML = "0";
+    document.getElementById("score1").innerHTML = "0";
+    document.getElementById("score2").innerHTML = "0";
+    document.querySelector('h1').innerHTML = "";
 }
